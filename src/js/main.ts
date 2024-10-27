e27a17e7e16b1c9cff83469641acd14135eeff04
unknown <d.kazarov1361@gmail.com>
2024-10-27 22:58:53 +0200

    hero: add text slider

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,3 +1,26 @@
 import burgerClick from './js-components/burger';
 
+import Swiper from 'swiper';
+import { Navigation, Autoplay, Pagination } from 'swiper/modules';
+
+const heroTextSlider = new Swiper('.hero__content', {
+	// configure Swiper to use modules
+	modules: [Navigation, Autoplay, Pagination],
+
+	autoplay: {
+		delay: 5000,
+	},
+
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true,
+	},
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+
 burgerClick();
