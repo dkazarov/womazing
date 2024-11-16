626110c9368b1d6d7d1b521afb43909f861d7ca9
unknown <d.kazarov1361@gmail.com>
2024-11-17 00:29:47 +0200

    header: add burger menu

--- src/js/js-components/burger.ts
+++ src/js/js-components/burger.ts
@@ -1,11 +1,27 @@
 const burger = document.querySelector('.burger');
 const burgerLine = document.querySelector('.burger__line');
+const nav = document.querySelector('.nav');
+const cart = document.querySelector('.cart');
+const body = document.querySelector('body');
+const navItem = document.querySelectorAll('.nav__item');
 
-const burgerClick = (): void => {
+export const burgerOpen = (): void => {
 	burger.addEventListener('click', () => {
 		burgerLine.classList.toggle('line--hide');
 		burger.classList.toggle('burger--transform');
+		nav.classList.toggle('overlay');
+		cart.classList.toggle('overlay');
+		body.classList.toggle('stop-scroll');
 	});
 };
-
-export default burgerClick;
+export const burgerClose = (): void => {
+	navItem.forEach(element => {
+		element.addEventListener('click', () => {
+			burgerLine.classList.remove('line--hide');
+			burger.classList.remove('burger--transform');
+			nav.classList.remove('overlay');
+			cart.classList.remove('overlay');
+			body.classList.remove('stop-scroll');
+		});
+	});
+};
