bebd8c60dba498dae0d69391f840545a93f3813d
unknown <d.kazarov1361@gmail.com>
2024-11-17 15:43:04 +0200

    hero: add global markup

--- src/js/js-components/heroSlider.ts
+++ src/js/js-components/heroSlider.ts
@@ -2,13 +2,8 @@ import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 
 export const leftHeroSlider = () => {
-	new Swiper('.swiper', {
-		modules: [Navigation, Pagination],
-
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
+	new Swiper('.hero-left__slider', {
+		modules: [Pagination],
 	});
 };
 export const rightHeroSlider = () => {
